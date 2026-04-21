9c358f39e225ef9bacb1b5b1bbec45be50bcef21
Tusiime Parvin Ruth <parvin.t.ruth@gmail.com>
2026-04-21 15:21:34 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <div>
+    <header class="header">
+      <h3>ParkEase</h3>
+      <nav class="nav">
+        <RouterLink to="/">Home</RouterLink> 
+        <RouterLink to="/about">About Us</RouterLink>
+      </nav>
+    </header>
+    <main>
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<style scoped></style>
